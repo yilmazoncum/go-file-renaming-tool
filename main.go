84c767a03caf5b1fe696b87b87760f8c0e1e4bdf
yilmazoncum <oncumkorkmaz@gmail.com>
2022-10-26 00:39:40 +0300

    nonrecursive renaming

--- main.go
+++ main.go
@@ -2,20 +2,57 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	fileName := "birthday_001.txt"
+	/* fileName := "birthday_001.txt"
 
 	newName, err := match(fileName, 4)
 	if err != nil {
 		fmt.Println("no match")
 		os.Exit(1)
 	}
-	fmt.Println(newName)
+	fmt.Println(newName) */
+	dir := "./sample"
+	count := 0
+	var toRename []string
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+		} else {
+			_, err := match(file.Name(), 0)
+			if err == nil {
+				count++
+				toRename = append(toRename, file.Name())
+			}
+		}
+	}
+
+	for _, originalFileName := range toRename {
+		originalPath := filepath.Join(dir, originalFileName)
+		newFileName, _ := match(originalFileName, count)
+		if err != nil {
+			panic(err)
+		}
+		newPath := filepath.Join(dir, newFileName)
+		fmt.Printf("mv %s => %s\n", originalPath, newPath)
+		err := os.Rename(originalPath, newPath)
+		if err != nil {
+			panic(err)
+		}
+
+	}
+
 }
 
 func match(fileName string, total int) (string, error) {
